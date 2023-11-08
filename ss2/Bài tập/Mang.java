0d8e1f4776a5270e5edfe4e2b94fdd675d6be509
tnhun <tnhung2207@gmail.com>
2023-11-08 23:21:25 +0700

    demo

---
+++ ss2/Bài tập/Mang.java
@@ -0,0 +1,11 @@
+package ss3;
+
+import java.util.Scanner;
+
+public class Mang {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Tên của bạn:" + name );
+    }
+}
